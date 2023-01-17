9dea5abb15a8da819935d1327c4b8b4cce0ba8fb
zsorosz <oroszi.zsofi@gmail.com>
2023-01-17 12:37:20 +0100

    Iteration-2 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -5,7 +5,10 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) printTimeCallback();
+    }, 1000);
   }
 
   getMinutes() {
